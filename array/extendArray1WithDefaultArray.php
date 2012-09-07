bf1cd71238d822b2a48508248acbde94ad0c12e2
sleibelt <stev.leibelt@jobleads.de>
2012-09-07 12:04:00 +0200

    Added timing and implemented comparison to array_merge

--- array/extendArray1WithDefaultArray.php
+++ array/extendArray1WithDefaultArray.php
@@ -11,11 +11,23 @@ $defaultOptions = array(
   'myOption3' => 'defaultThree'
 );
 
+$time = array();
+
+$time['extendedArray']['start'] = microtime();
 $extendedArray = extendOptionsWithDefaultOptions($options, $defaultOptions);
+$time['extendedArray']['end'] = microtime() - $time['extendedArray']['start'];
+$time['mergedArray']['start'] = microtime();
 $mergedArray = array_merge($options, $defaultOptions);
+$time['mergedArray']['end'] = microtime() - $time['mergedArray']['start'];
+$time['mergedArrayTwo']['start'] = microtime();
+$mergedArrayTwo = array_merge($defaultOptions, $options);
+$time['mergedArrayTwo']['end'] = microtime() - $time['mergedArrayTwo']['start'];
 
 echo 'extendedArray' . PHP_EOL . xdebug_var_dump($extendedArray) . PHP_EOL;
 echo 'mergedArray' . PHP_EOL . xdebug_var_dump($mergedArray) . PHP_EOL;
+echo 'mergedArrayTwo' . PHP_EOL . xdebug_var_dump($mergedArrayTwo) . PHP_EOL;
+echo '----------------' . PHP_EOL;
+echo 'times' . PHP_EOL . xdebug_var_dump($time) . PHP_EOL;
 echo '----------------' . PHP_EOL;
 echo 'options' . PHP_EOL . xdebug_var_dump($options) . PHP_EOL;
 echo 'defaultOptions' . PHP_EOL . xdebug_var_dump($defaultOptions) . PHP_EOL;
