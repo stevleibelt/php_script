3ef723c67973771eb41e2b280a0c1bc6361b89ac
stevleibelt <max.mustermann@jobleads.de>
2012-09-28 16:39:48 +0200

    Added example for "to the power of"

---
+++ math/powerOf.php
@@ -0,0 +1,6 @@
+<?php
+
+echo '2^8: ' . pow(2, 8) . PHP_EOL;
+echo '1024^1 (KiloByte): ' . pow(1024, 1) . PHP_EOL;
+echo '1024^2 (MegaByte): ' . pow(1024, 2) . PHP_EOL;
+echo '1024^3 (GigaByte): ' . pow(1024, 3) . PHP_EOL;
