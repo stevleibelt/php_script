7293d78ceed6bd0482accd251610c6a27ebbd130
artodeto <artodeto@arcor.de>
2013-01-08 10:35:42 +0100

    Added example.

--- array/arrayMerge.php
+++ array/arrayMerge.php
@@ -13,13 +13,36 @@ $array2 = array(
   666 => 'the number of the beast'
 );
 
+$array3 = array(
+  'foo',
+  'bar',
+  'foobar'
+);
+
+$array4 = array(
+  'foo',
+  'bar',
+  'barfoo'
+);
+
 echo PHP_EOL . 'array1' . PHP_EOL;
 echo var_export($array1, true);
 echo PHP_EOL . 'array2' . PHP_EOL;
 echo var_export($array2, true);
+echo PHP_EOL . 'array3' . PHP_EOL;
+echo var_export($array3, true);
+echo PHP_EOL . 'array4' . PHP_EOL;
+echo var_export($array4, true);
 echo PHP_EOL . '--------';
+
 echo PHP_EOL . 'arrays merged (array1, array2)' . PHP_EOL;
 echo var_export(array_merge($array1, $array2), true);
 echo PHP_EOL . '--------';
+
 echo PHP_EOL . 'arrays merged (array2, array1)' . PHP_EOL;
 echo var_export(array_merge($array2, $array1), true);
+echo PHP_EOL . '--------';
+
+echo PHP_EOL . 'arrays merged (array3, array4)' . PHP_EOL;
+echo var_export(array_merge($array3, $array4), true);
+echo PHP_EOL . '--------';
