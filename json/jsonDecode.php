8bbf89503df708773b8b8e2dba31b7cb915884f4
artodeto <artodeto@arcor.de>
2013-01-11 12:21:18 +0100

    Added example for json.

---
+++ json/jsonDecode.php
@@ -0,0 +1,25 @@
+<?php
+/**
+* Example for json encoding.
+*
+* @author Stev Leibelt <artodeto@arcor.de>
+* @since 2013-01-11
+*/
+
+$jsonContent = array(
+  'int' => 13,
+  'string' => 'one two three',
+  'stringWithUmlauts' => '"\\u00dcberzone"',
+  'array' => '{"string":"one","int":23}'
+);
+
+foreach ($jsonContent as $type => $content) {
+  echo 'type::' . $type . PHP_EOL;
+  echo 'content' . PHP_EOL;
+  echo var_export($content, true);
+  echo PHP_EOL;
+  echo 'decoded from json' . PHP_EOL;
+  echo var_export(json_decode($content), true);
+  echo PHP_EOL;
+  echo '----' . PHP_EOL;
+}
