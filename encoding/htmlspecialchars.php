fec988c1dc3cbd81af26d140c1ae7f374f645946
artodeto <artodeto@arcor.de>
2013-02-07 17:27:50 +0100

    Fixed errors.

---
+++ encoding/htmlspecialchars.php
@@ -0,0 +1,12 @@
+<?php
+
+$htmlContent = array(
+  '<a href="foo/bar">foobar</a>',
+  'http://www.foo.bar/name\foo\Bar'
+);
+
+foreach ($htmlContent as $content) {
+    echo $content . PHP_EOL;
+    echo htmlspecialchars($content) . PHP_EOL;
+    echo PHP_EOL;
+}
