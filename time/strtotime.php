915d0bb15ee8c8b4b3ec24b82102e66a1c8d935f
stevleibelt <stev@leibelt.de>
2013-05-03 17:15:33 +0200

    Adapted example to show problem when dealing with februrary

--- time/strtotime.php
+++ time/strtotime.php
@@ -7,6 +7,9 @@
 */
 
 $examples = array(
+  '+1 month',
+  '10 month',
+  '-1 month',
   '+1 minutes',
   '10 minutes',
   '-1 minutes',
@@ -23,11 +26,16 @@ $examples = array(
 
 foreach ($examples as $example) {
   $time = time();
+  $time = strtotime('2013-01-31');
+  $datetime = new DateTime('2013-01-31');
 
   echo 'example:: "' . $example . '"' . PHP_EOL;
   echo 'strtotime as date for example:: "' . date('Y-m-d H:i:s', strtotime($example, $time)) . '"' . PHP_EOL;
   echo 'strtotime as date for now:: "' . date('Y-m-d H:i:s', $time) . '"' . PHP_EOL;
   echo 'strtotime:: "' . strtotime($example, $time) . '"' . PHP_EOL;
   echo 'current timestamp:: "' . $time . '"' . PHP_EOL;
+  echo 'datetime:: '  . $datetime->format('y-m-d H:i:s') . PHP_EOL;
+  $datetime->modify($example);
+  echo 'datetime modified:: '  . $datetime->format('y-m-d H:i:s') . PHP_EOL;
   echo PHP_EOL;
 }
