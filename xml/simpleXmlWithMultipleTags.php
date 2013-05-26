9616efe1557294f5918ef7a2e6dd647d47d67f74
stevleibelt <stev@leibelt.de>
2013-05-26 16:53:08 +0200

    Added examples for file and xml

---
+++ xml/simpleXmlWithMultipleTags.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Example how to iterate over a xml with multiple entries of same tag
+ *
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-05-25
+ */
+
+$xmlContent = <<<EOXML
+<?xml version="1.0" encoding="utf-8" ?>
+<example>
+    <multipleTag>
+        first entry
+    </multipleTag>
+    <multipleTag>
+        second entry
+    </multipleTag>
+    <multipleTag>
+        third entry
+    </multipleTag>
+</example>
+EOXML;
+
+$xmlObject = new SimpleXMLElement($xmlContent);
+
+echo var_export((array) $xmlObject->multipleTag, true);
