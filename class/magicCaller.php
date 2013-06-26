ab927ea281f49e0d28e41046aaf5d025f19dd53a
stevleibelt <stev@leibelt.de>
2013-06-26 22:08:52 +0200

    Added examples for magic methods

---
+++ class/magicCaller.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-06-26
+ */
+
+class MyClass
+{
+        public function __call($name, $value)
+        {
+            echo 'called ' . __METHOD__ . ' with name: "' . $name . 
+                '" and values: "' . implode(',' $value) . '"' . PHP_EOL;
+        }
+}
+
+$myClass = new MyClass();
+
+//$myClass->foo = 'bar';  //not working, take a look to magicSetter.php
+$myClass->setBar('foo');    //working
---
+++ class/magicSetter.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-06-26
+ */
+
+class MyClass
+{
+        public function __set($name, $value)
+        {
+            echo 'called ' . __METHOD__ . ' with name: "' . $name . 
+                '" and value: "' . $value . '"' . PHP_EOL;
+        }
+}
+
+$myClass = new MyClass();
+
+$myClass->foo = 'bar';  //working
+//$myClass->setBar('foo');    //not working, take a look to magicCaller.php
