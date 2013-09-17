7de62293260313a7f6b9e357627ffac6873eed9e
stevleibelt <stev@leibelt.de>
2013-09-18 00:24:44 +0200

    Added

---
+++ math/moduloAndTimestamp.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-09-18
+ */
+
+//available seconds
+$seconds = array(1, 2, 3, 4, 6, 9, 15);
+$initialTime = time();
+//number of loops
+$loops = 53;
+
+echo str_repeat('-', 40) . PHP_EOL;
+echo 'initial time ' . $initialTime . PHP_EOL;
+echo str_repeat('-', 40) . PHP_EOL;
+
+//@todo what happens if sleep is two or three seconds?
+
+for($i=0;$i<$loops;$i++) {
+    $currentTime = time();
+    $timeDifference = $currentTime - $initialTime;
+
+    echo 'current loop: ' . ($i+1) . '/' . $loops . PHP_EOL;
+    echo str_repeat('-', 20) . PHP_EOL;
+    echo 'current time ' . $currentTime . PHP_EOL;
+    echo 'time difference ' . $timeDifference . PHP_EOL;
+    echo str_repeat('-', 20) . PHP_EOL;
+
+    foreach ($seconds as $second) {
+        $isTriggered = (($timeDifference % $second) === 0);
+        echo $second . ' is triggered ' . ($isTriggered ? 'yes' : 'no') . PHP_EOL;
+    }
+    echo str_repeat('-', 40) . PHP_EOL;
+    sleep(1);
+}
