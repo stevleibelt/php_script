9a3bcfe5516c880facd627b5d8c06067a8289078
stevleibelt <stev@leibelt.de>
2013-10-16 17:29:40 +0200

    Addd explode example

---
+++ array/explode.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author stev leibetl <artodeto@arcor.de>
+ * @since 2013-10-16
+ */
+
+$string = 'foo || bar || foobar';
+$array = explode(' || ', $string);
+
+echo var_export($array, true) . PHP_EOL;
