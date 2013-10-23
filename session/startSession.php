f2421b0ee470baca5bedb8eb4a98f5e398db63ab
stevleibelt <stev@leibelt.de>
2013-10-23 11:53:40 +0200

    Added session directory

---
+++ session/startSession.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-10-23
+ */
+
+/**
+ * Class Session
+ *
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-10-23
+ */
+class Session
+{
+    /**
+     * @return bool
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-10-23
+     */
+    public function isStarted()
+    {
+        if (version_compare(PHP_VERSION, '5.4.0') >= 0) {
+            $isStarted = !(session_status() == PHP_SESSION_NONE);
+        } else {
+            $isStarted = !(is_null($_SESSION));
+        }
+    }
+}
