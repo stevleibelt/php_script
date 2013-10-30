8dcd9ef3e2ce8a2239cf09ad3ac153429c7b673d
stevleibelt <stev@leibelt.de>
2013-10-30 17:19:00 +0100

    Fixed bug and added content

--- array/memoryUsageOfABigIntArray.php
+++ array/memoryUsageOfABigIntArray.php
@@ -6,8 +6,8 @@ $endId = 500000;
 echo 'Memory usage: ' . round(memory_get_usage(true)/1048576,2) . ' mb' . PHP_EOL;
 echo 'Adding entries to array' . PHP_EOL;
 for ($iterator = 1; $iterator <= $endId; $iterator++) {
+    $array[] = $iterator;
     if ($iterator % 500 === 0) {
-        $array[] = $iterator;
         echo '.';
     }
     if ($iterator % 50000 === 0) {
@@ -15,4 +15,5 @@ for ($iterator = 1; $iterator <= $endId; $iterator++) {
     }
 }
 echo PHP_EOL;
+echo 'Array contains ' . count($array) . ' entries' . PHP_EOL;
 echo 'Memory usage: ' . round(memory_get_usage(true)/1048576,2) . ' mb' . PHP_EOL;
