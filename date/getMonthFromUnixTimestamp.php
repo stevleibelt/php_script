258e47263e01a9e153c92b210b34ef9666d03d1a
stevleibelt <stev@leibelt.de>
2013-12-16 18:46:33 +0100

    Added getMonthFromUnixTimestamp

---
+++ date/getMonthFromUnixTimestamp.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-12-16
+ */
+
+$time = time();
+$month = date('m', $time);
+
+echo 'unix timestamp: ' . $time . PHP_EOL;
+echo 'month: ' . $month . PHP_EOL;
