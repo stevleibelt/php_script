3748aa4f25e8d455423122952211db7d2874b99b
stevleibelt <stev@leibelt.de>
2013-12-19 16:48:10 +0100

    Added strToTime

---
+++ date/strToTime.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-12-19
+ */
+
+$beginArray = array(
+    '1970',
+    '1970-01',
+    '1970-01-01',
+    '1983-10-06',
+    '2008-08-04'
+);
+$beginToTime = array();
+
+foreach ($beginArray as $begin) {
+    $beginToTime[$begin] = strtotime($begin);
+}
+
+foreach ($beginToTime as $begin => $time) {
+    echo 'begin: ' . $begin . PHP_EOL;
+    echo 'time: ' . $time . PHP_EOL;
+    echo 'date(Y-m-d H:i:s): ' . date('Y-m-d H:i:s', $time) . PHP_EOL;
+}
